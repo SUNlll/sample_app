f2204c72ec59185b9a9752b70986f424de54f079
SUNlll <sunhao@dowedo.com.cn>
2016-08-16 15:12:05 +0800

    Serve static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,14 +84,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-
-
-
-end
-SampleApp::Application.configure do
-
-
   config.serve_static_assets =true
 
 
 end
+
